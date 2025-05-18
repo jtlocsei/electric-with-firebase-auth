530112a521eaa3d3f1a1e562b08f022b612cbee2
Tobias Locsei <jtlocsei@gmail.com>
2025-05-19 07:30:22 +0800

    re-order code in src/electric_starter_app/firebase_client.cljs for clarity

--- src/electric_starter_app/firebase_client.cljs
+++ src/electric_starter_app/firebase_client.cljs
@@ -32,51 +32,10 @@
   (GoogleAuthProvider.))
 
 
-;; Track the sign in state and save the user object in the client db
-;; https://firebase.google.com/docs/auth/web/start#set_an_authentication_state_observer_and_get_user_data
-(defonce !auth-state-change-listener-added? (atom false))
-(when-not @!auth-state-change-listener-added?
-  (do
-    (onAuthStateChanged firebase-auth
-      (fn [user]
-        (if user
-          (do
-            (js/console.log "Auth state changed. Adding user")
-            (swap! !client-db db/set-user user))
-          (do
-            (js/console.log "Auth state changed. Removing user")
-            (swap! !client-db db/remove-user)))))
-    (reset! !auth-state-change-listener-added? true)))
-(comment
-  ; User's email
-  (some-> (db/get-user @!client-db) .-email)
 
-  ; Print user object to console
-  (js/console.log (db/get-user @!client-db))
-
-  ;; We can also see the current user using the currentUser property (nil if no-one logged in)
-  (.-currentUser firebase-auth)
-  :_)
-
-
-;; Keep track of the latest id token, which Firebase auto-refreshes every hour or so while logged in
-;; https://firebase.google.com/docs/reference/js/v8/firebase.auth.Auth#onidtokenchanged
-(defonce !token-change-listener-added? (atom false))
-(when-not @!token-change-listener-added?
-  (onIdTokenChanged firebase-auth
-    (fn [user]
-      (if user
-        (do
-          (-> (.getIdToken user)
-            (.then (fn [id-token]
-                     (swap! !client-db db/set-id-token id-token)))
-            (.catch (fn [error]
-                      (js/console.log "Error with getIDToken" error))))
-          (js/console.log "ID token changed. User is logged in"))
-        (do
-          (swap! !client-db db/remove-id-token)
-          (js/console.log "ID token changed. User logged out")))))
-  (reset! !token-change-listener-added? true))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Sign In, Sign Out, Create User
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;; Sign in a user with Google
@@ -97,7 +56,7 @@
   :_)
 
 
-;; Sign out the current user (cljs)
+;; Sign out the current user
 ;; https://firebase.google.com/docs/auth/web/password-auth (scroll to bottom)
 (defn sign-out
   []
@@ -137,14 +96,15 @@
   :_)
 
 
-;; Print ID token of current user
-(comment
-  (-> (.getIdToken (db/get-user @!client-db))
-    (.then (fn [id-token]
-             (println id-token)))
-    (.catch (fn [error]
-              (js/console.log "Error with getIdToken of user" error))))
-  :_)
+
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ID Token Automatic Refresh
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (defn refresh-id-token []
@@ -174,3 +134,71 @@
     refresh-id-token
     (* 1000 60 5))
   (reset! !token-refresh-timer-added? true))
+
+
+;; Print current ID token
+(comment
+  (-> (.getIdToken (db/get-user @!client-db))
+    (.then (fn [id-token]
+             (println id-token)))
+    (.catch (fn [error]
+              (js/console.log "Error with getIdToken of user" error))))
+  :_)
+
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Track User and Token in Client DB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+; Track the sign in state and save the user object in the client db
+;; https://firebase.google.com/docs/auth/web/start#set_an_authentication_state_observer_and_get_user_data
+(defonce !auth-state-change-listener-added? (atom false))
+(when-not @!auth-state-change-listener-added?
+  (do
+    (onAuthStateChanged firebase-auth
+      (fn [user]
+        (if user
+          (do
+            (js/console.log "Auth state changed. Adding user")
+            (swap! !client-db db/set-user user))
+          (do
+            (js/console.log "Auth state changed. Removing user")
+            (swap! !client-db db/remove-user)))))
+    (reset! !auth-state-change-listener-added? true)))
+(comment
+  ; User's email
+  (some-> (db/get-user @!client-db) .-email)
+
+  ; Print user object to console
+  (js/console.log (db/get-user @!client-db))
+
+  ;; We can also see the current user using the currentUser property (nil if no-one logged in)
+  (.-currentUser firebase-auth)
+  :_)
+
+
+;; Keep track of the latest id token when it changes
+;; https://firebase.google.com/docs/reference/js/v8/firebase.auth.Auth#onidtokenchanged
+(defonce !token-change-listener-added? (atom false))
+(when-not @!token-change-listener-added?
+  (onIdTokenChanged firebase-auth
+    (fn [user]
+      (if user
+        (do
+          (-> (.getIdToken user)
+            (.then (fn [id-token]
+                     (swap! !client-db db/set-id-token id-token)))
+            (.catch (fn [error]
+                      (js/console.log "Error with getIDToken" error))))
+          (js/console.log "ID token changed. User is logged in"))
+        (do
+          (swap! !client-db db/remove-id-token)
+          (js/console.log "ID token changed. User logged out")))))
+  (reset! !token-change-listener-added? true))
+
+
